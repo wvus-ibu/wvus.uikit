3054080a833ed7e7a21e06e0b78d5fc5df926500
Ryan Wooten <rwooten@worldvision.org>
2013-07-02 12:19:31 -0700

    Updated the script to suppress output from rm command

--- src/update.sh
+++ src/update.sh
@@ -6,18 +6,18 @@ echo
 echo "Updating Bootstrap..."
 rm -Rf lib/bootstrap >/dev/null
 volo add -nostamp bootstrap lib/bootstrap
-rm -f lib/bootstrap/* >/dev/null
+rm -f lib/bootstrap/* &>/dev/null
 echo "Bootstrap successfully updated"
 echo 
 echo "Updating Font Awesome..."
 #font-awesome
 rm -Rf lib/font-awesome >/dev/null
 volo add -nostamp font-awesome lib/font-awesome
-rm -f lib/font-awesome/* >/dev/null
+rm -f lib/font-awesome/* &>/dev/null
 echo "Font Awesome successfully updated"
 echo 
 echo "Updating jQuery..."
-rm -Rf lib/jquery/*.* >/dev/null
+rm -Rf lib/jquery/*.* &>/dev/null
 volo add -nostamp jquery lib/jquery/jquery.js
 echo "jQuery successfully updated"
 echo
