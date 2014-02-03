a10f8d95d0561dedcc3e398538d005afde1fcc11
Ryan Wooten <rwooten@worldvision.org>
2014-02-03 09:58:22 -0800

    Adding a version function to the uikit js.  Also documenting it

--- src/lib/jquery/noconflict-jquery.js
+++ src/lib/jquery/noconflict-jquery.js
@@ -1,7 +1,6 @@
 /* Establishes wvusUikit as the object to use with UiKit JS plugins*/
 var wvusUikit = jQuery.noConflict(true);
 
-var version = "@VERSION";
 wvusUikit.fn.extend({
-  version: version,
+  version: "@VERSION",
 });
